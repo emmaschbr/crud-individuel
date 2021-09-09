7510a8cd9e38e0c6420634638d3cee4d7eb13be7
emma.scheuber <emma.scheuber@gmail.com>
2021-09-09 12:45:36 +0200

    delete app mobile

--- applimobile/android/app/src/main/kotlin/com/example/applimobile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.applimobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- applimobile/lib/page/contact.dart
+++
@@ -1,6 +0,0 @@
-class Contact {
-  String nom;
-  String imageProfil;
-
-  Contact({ required this.nom, required this.imageProfil });
-}
